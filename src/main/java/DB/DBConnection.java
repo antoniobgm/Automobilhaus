77ea2de9a4df63579bdd119086d443e170ceb9b4
anton <antoniobernd.gm@gmail.com>
2021-06-21 23:29:37 +0200

    initial commit

--- src/main/java/DB/DBConnection.java
+++ src/main/java/DB/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
     public Connection getConnection() throws SQLException{
-       Connection connection=DriverManager.getConnection("jdbc:h2:file:./Datenbank123");
+       Connection connection=DriverManager.getConnection("jdbc:h2:file:./Datenbank");
        return connection;
     }
 }
